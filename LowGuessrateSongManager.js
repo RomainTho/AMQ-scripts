01f7299199ef2d1f87944891dc4c27dab89def85
Romain THO <romain.tho@etu.univ-cotedazur.fr>
2024-08-16 19:28:53 +0200

    add delete last song button + update before deleting

--- LowGuessrateSongManager.js
+++ LowGuessrateSongManager.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Low Guess Rate Songs Manager
 // @namespace    https://github.com/RomainTho/AMQ-scripts
-// @version      1.1
+// @version      1.2
 // @description  Manage low Guess Rate songs by number with a simple interface.
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqWindows.js
@@ -55,6 +55,7 @@ function updateUI() {
 
 // Deletes a song by its number
 function deleteSongByNumber(number) {
+    updateUI(); // Ensure the UI is updated before deleting
     if (isNaN(number) || number < 1 || number > songs.length) {
         gameChat.systemMessage("Invalid song number.");
         return;
@@ -65,6 +66,19 @@ function deleteSongByNumber(number) {
     gameChat.systemMessage("Song number " + number + " deleted.");
 }
 
+// Deletes the last song added
+function deleteLastSong() {
+    updateUI(); // Ensure the UI is updated before deleting
+    if (songs.length === 0) {
+        gameChat.systemMessage("No songs to delete.");
+        return;
+    }
+
+    songs.pop(); // Remove the last song
+    localStorage.setItem("lowGuessRateSongs", JSON.stringify(songs));
+    gameChat.systemMessage("Last song deleted.");
+}
+
 let managerWindow;
 
 // Creates the manager window for the UI
@@ -73,7 +87,7 @@ function createSongsManagerWindow() {
         id: "songsManagerWindow",
         title: "Low Guess Rate Songs Manager",
         width: 400, // Adjusted width
-        height: 300, // Adjusted height
+        height: 340, // Increased height to fit new button
         zIndex: 1054,
         draggable: true
     });
@@ -108,6 +122,17 @@ function createSongsManagerWindow() {
             x: 0,
             y: 125,
         },
+        id: "deleteLastButton"
+    });
+
+    managerWindow.addPanel({
+        width: 1.0,
+        height: 40, // Increased height for better visibility
+        zIndex: 1058,
+        position: {
+            x: 0,
+            y: 175,
+        },
         id: "showButton"
     });
 
@@ -127,6 +152,15 @@ function createSongsManagerWindow() {
     );
 
     managerWindow.panels[2].panel.append(
+        $(`<div id="deleteLastButton"></div>`)
+        .append(
+            $(`<button id="deleteLastButton" class="btn btn-primary">Delete Last Song</button>`).click(function () {
+                deleteLastSong();
+            })
+        )
+    );
+
+    managerWindow.panels[3].panel.append(
         $(`<div id="showButton"></div>`)
         .append(
             $(`<button id="showButton" class="btn btn-primary">Show All Songs</button>`).click(function () {
@@ -175,26 +209,23 @@ AMQ_addStyle(`
         border: #2E2E2E;
         font-size: 14px; /* Text size */
     }
-    #deleteButton {
+    #deleteButton, #deleteLastButton, #showButton {
         width: 350px; /* Adjusted width */
         text-align: center;
+    }
+    #deleteButton > button, #deleteLastButton > button, #showButton > button {
+        text-align: center;
+        width: 90%;
+        margin: 5px 15px;
+        font-size: 14px;
     }
     #deleteButton > button {
-        text-align: center;
-        width: 90%;
-        margin: 5px 15px;
         background-color: #FF0000; /* Button color */
-        font-size: 14px;
     }
-    #showButton {
-        width: 350px; /* Adjusted width */
-        text-align: center;
+    #deleteLastButton > button {
+        background-color: #FF4500; /* Button color */
     }
     #showButton > button {
-        text-align: center;
-        width: 90%;
-        margin: 5px 15px;
         background-color: #70AAE5; /* Button color */
-        font-size: 14px;
     }
 `);
